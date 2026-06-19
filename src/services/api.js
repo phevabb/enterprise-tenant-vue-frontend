343a099505f7fea1731d855574bb393daa874d0c
phevabb <phevab1@gmail.com>
2026-06-19 09:22:50 +0000

    baseurl updaates

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-   baseURL: 'http://127.0.0.1:9001/api/', // dev server
- //  baseURL: 'https://kogschool.com/enterprise/api/',
+   // baseURL: 'http://127.0.0.1:9001/api/', // dev server
+ baseURL: 'https://kogschool.com/enterprise/api/',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
